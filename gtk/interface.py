d6960e27e3e8ab31fe1bb3c3f0dca8bb294123cd
pmqueiroz <pedroqueiroz593@gmail.com>
2020-07-05 11:39:06 -0300

    Gtk

--- gtk/interface.py
+++ gtk/interface.py
@@ -1,2 +1,33 @@
+import gi
+
+gi.required_version("Gtk", "3,0")
+from gi.repository import Gtk
+
+class Handler:
+    def onDestroy(self, *args):
+        Gtk.main_quit()
+
+    def onButtonPressed(self, button):
+        print("Button pressed")
+
 builder = Gtk.Builder()
-builder.add_from_file
+builder.add_from_file("interface.glade")
+builder.connect_signals(Handler())
+
+window = builder.get_object("interface_window")
+window.show_all()
+
+Gtk.main()
+
+""" def __init__(self):
+    gtk.Window.__init__(self)
+    self.set_border_width(10)
+
+    self.connect('delete-event', self.cb_delete_event)
+
+    self.connect('destroy', lambda *w: gtk.main_quit())
+
+if __name__ == "__main__":
+    win = Sample()
+    win.show_all()
+    gtk.main() """
